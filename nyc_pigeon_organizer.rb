3f562e0536a1ff6981f778b0bda0d03667d3eb25
Jacob Kenny <jacobkenny05@gmail.com>
2019-12-04 22:36:24 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  #create the result hash
+  final_hash = {}
+  data.each do |keys, value|
+    #keys is :color, :gender, :lives 
+    #value is hashes of 4 different colors with arrays of pigeon names for each color. 2 genders with array of pigeon names for each gender. 4 locations with arrays of pigeon names for each loction.  
+    #binding.pry 
+   value.each do |info, pigeon_names|
+     #info is purple, grey, white, brown, male, female, Subway, Central Park, Library, City Hall 
+     #pigeon_names is arrays of pigeon names for the classes of info
+     #binding.pry 
+     pigeon_names.each do |n|
+       #n is individal pigeon names in the arrays of the above-mentioned pigeon_names 
+       #binding.pry 
+       if !final_hash[n] 
+         final_hash[n] = {}
+       end 
+       #binding.pry 
+       if !final_hash[n][keys] 
+         final_hash[n][keys] = []
+       end
+       #binding.pry 
+       final_hash[n][keys] << (info.to_s)
+       #binding.pry
+     end 
+   end 
+  end
+  #binding.pry
+  final_hash 
 end
